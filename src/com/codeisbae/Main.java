20694a05e4670c55aa2477d5cf4f350109a2a2c4
fuchodeveloper <fmgbeoma@yahoo.com>
2020-11-01 16:38:37 +0100

    Minutes to Years and Days calculator

--- src/com/codeisbae/Main.java
+++ src/com/codeisbae/Main.java
@@ -2,25 +2,47 @@ package com.codeisbae;
 
 public class Main {
 
-    public static void main(String[] args) {
-	// write your code here
+  public static void main(String[] args) {
+    // write your code here
+    printYearsAndDays(561600);
+  }
+
+  // Minutes to Years and Days calculator
+  public static void printYearsAndDays(long minutes) {
+    if (minutes < 0) {
+      System.out.println("Invalid value");
+    } else {
+      /*
+       1 hour = 60 minutes
+       1 day = 24 hours
+       1 year = 365 days
+      */
+      long h = minutes / 60;
+      long d = h / 24;
+      long y = d / 365;
+      long yRemainder = d % 365;
+      System.out.println(minutes + " min = " + y + " y and " + yRemainder + " d");
     }
+  }
 
-    // Area Calculator
-    private static final double PI = Math.PI;
-    public static double area(double radius) {
-        if ((radius < 0)) return -1.0;
+  // Area Calculator
+  private static final double PI = Math.PI;
 
-        return radius * radius * PI;
-    }
+  public static double area(double radius) {
+    if ((radius < 0)) return -1.0;
 
-    public static double area(double x, double y) {
-        if ((x < 0 || y < 0)) return -1.0;
+    return radius * radius * PI;
+  }
 
-        return x * y;
-    }
-    //        System.out.println(area(5.0));
-    //        System.out.println(area(-1));
-    //        System.out.println(area(5.0, 4.0));
-    //        System.out.println(area(-1.0,4.0));
+  public static double area(double x, double y) {
+    if ((x < 0 || y < 0)) return -1.0;
+
+    return x * y;
+  }
+    /*
+      System.out.println(area(5.0));
+      System.out.println(area(-1));
+      System.out.println(area(5.0, 4.0));
+      System.out.println(area(-1.0,4.0));
+    */
 }
